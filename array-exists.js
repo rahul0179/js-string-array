8462e4e207952352634b9d240c806cc5d9710302
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-07-24 08:06:45 +0600

    2nd commit

---
+++ array-exists.js
@@ -0,0 +1,22 @@
+function megaFriend(friends) {
+    if (Array.isArray(friends) == false) {
+        return 'please provide right array';
+    }
+    let mega = friends[0];
+    for (const i of friends) {
+        if (i.length > mega.length)
+            mega = i;
+    }
+    return mega;
+
+}
+
+const friends = ['abul', 'nakul', 'kabul', 'ratul'];
+const myBigGuy = megaFriend(36556);
+console.log(myBigGuy);
+if (friends.indexOf('abul' != -1)) {
+    console.log("abul ace");
+}
+if (friends.includes('ratul')) {
+    console.log("ace");
+}
