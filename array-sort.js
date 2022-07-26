313cb9d7edf7f5a8b40f1fae38d69ab94ac00fa3
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-07-26 19:28:51 +0600

    comit

---
+++ array-sort.js
@@ -0,0 +1,7 @@
+let number = [2, 6, 3, 8, 1, 5, 9, 4, 7];
+let sortNum = number.reverse();
+console.log(sortNum);
+
+let name = ['abul', 'kabul', 'babul', 'tabul'];
+let nameSort = name.reverse();
+console.log(nameSort);
